e388810a1e8bfa5d712382255a03ee54c3c14ed6
niccolozanotti <niccolozanotti@users.noreply.github.com>
2024-10-27 19:24:30 +0000

    deploy: 41af89c0afe15de6ade43d9aa11df9ccffbc69ff

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Romagna m-IA - Eventi sull'IA e Social media\n",'url':"/2024/10/27/romagnaIA.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Romagna m-IA - Eventi sull'IA e Social media\n",'url':"/2024/10/27/romagnamIA.html"}]};
